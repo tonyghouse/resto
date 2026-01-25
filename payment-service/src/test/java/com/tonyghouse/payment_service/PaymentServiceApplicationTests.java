acb5383a00223d145102f81f2520810aa5c22918
Tony Ghouse <tonyghouseofficial@gmail.com>
2026-01-25 13:52:24 +0530

    restaurant service and payment service are created from spring initialzer

---
+++ payment-service/src/test/java/com/tonyghouse/payment_service/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonyghouse.payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
