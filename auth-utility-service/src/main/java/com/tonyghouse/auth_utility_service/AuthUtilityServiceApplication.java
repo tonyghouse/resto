899b1457c8d6c9f878c5dad8726796de0b25a8cc
Tony Ghouse <tonyghouseofficial@gmail.com>
2026-01-25 15:11:06 +0530

    auth utility service added by spring iniatilizr

---
+++ auth-utility-service/src/main/java/com/tonyghouse/auth_utility_service/AuthUtilityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tonyghouse.auth_utility_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthUtilityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthUtilityServiceApplication.class, args);
+	}
+
+}
